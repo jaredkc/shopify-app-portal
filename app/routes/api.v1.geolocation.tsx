cc0f4c526c2a377eb49b53cccdbd1bcd7b4b739f
jaredkc <jaredkc@gmail.com>
2025-02-25 12:45:24 -0700

    correctly use one ip address

--- app/routes/api.v1.geolocation.tsx
+++ app/routes/api.v1.geolocation.tsx
@@ -4,23 +4,21 @@ import invariant from "tiny-invariant";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  console.log("Request headers:", request.headers);
-  console.log("IP address:", request.headers.get("x-forwarded-for"));
-
-  const { session } = await authenticate.public.appProxy(request);
-  const ip = request.headers.get("x-forwarded-for");
-
-
   invariant(process.env.MAXMIND_ACCOUNT_ID, "MAXMIND_ACCOUNT_ID is not set");
   invariant(process.env.MAXMIND_LICENSE_KEY, "MAXMIND_LICENSE_KEY is not set");
 
-  if (!ip) {
+  const { session } = await authenticate.public.appProxy(request);
+  const ipFromHeader = request.headers.get("x-forwarded-for");
+
+  if (!ipFromHeader) {
     return json({
       message: "No IP address found in request headers",
     });
   }
 
-  let message = `Geolocation based on your IP address ${ip}`;
+  const ipAddress = ipFromHeader.split(",")[0];
+
+  let message = `Geolocation based on your IP address ${ipAddress}`;
 
   // To query the GeoLite2 web service, you must set the optional `host` parameter
   const client = new WebServiceClient(
@@ -33,12 +31,12 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
   // You can also use `client.city` or `client.insights`
   // `client.insights` is not available to GeoLite2 users
-  const geolocation = await client.country(ip).then((response) => response);
-
-  console.log("Geolocation:", geolocation);
+  const geolocation = await client
+    .country(ipAddress)
+    .then((response) => response);
 
   if (geolocation && geolocation.country) {
-    message = `Geolocation based on your IP address ${ip} is not available`;
+    message = `Geolocation based on your IP address ${ipAddress} is not available`;
   }
 
   return json({
